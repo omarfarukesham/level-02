4ac2b390e2ab0e0a4a34847aa4f88c42c2e802a6
omar.lu86@gmail.com <omar.lu86@gmail.com>
2024-06-20 10:15:54 +0600

    feat: tuples and object literal practise done

---
+++ mission-01/module-02/src/Objects.ts
@@ -0,0 +1,18 @@
+// object decleared and literal
+
+interface Person{
+    name: string,
+    age: number,
+    phone: number,
+    address: string
+}
+
+const person:Person = {
+    name: "Isham",
+    age: 20,
+    phone: 1234455,
+    address: "DHaka"
+}
+
+
+console.log(person.address)
