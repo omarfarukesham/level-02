7e96de2ea0ce978cbac97e308a820cbb418aa74a
omar.lu86@gmail.com <omar.lu86@gmail.com>
2024-06-20 10:37:01 +0600

    feat: objects literal practise has done

--- mission-01/module-02/src/Objects.ts
+++ mission-01/module-02/src/Objects.ts
@@ -1,18 +1,69 @@
 // object decleared and literal
 
-interface Person{
+// interface Person{
+//     name: string,
+//     age: number,
+//     phone: number,
+//     address: string
+// }
+
+// const person:Person = {
+//     name: "Isham",
+//     age: 20,
+//     phone: 1234455,
+//     address: "DHaka"
+// }
+
+
+// console.log(person.address)
+
+//string literal
+// type Direction = "North" | "South" | "East";
+
+// let direction: Direction;
+
+// direction = "East";
+// console.log(direction)
+
+// interface Car{
+//     carName: string,
+//     carModal: string,
+//     price: number,
+//     year?: number,
+
+// }
+
+// const car: Car = {
+//     carName: 'Toyta',
+//     carModal: "2-x-serize",
+//     price: 200000000000,
+
+// }
+// console.log(car.carName)
+
+//combain example
+
+interface Products{
     name: string,
-    age: number,
-    phone: number,
-    address: string
-}
-
-const person:Person = {
-    name: "Isham",
-    age: 20,
-    phone: 1234455,
-    address: "DHaka"
+    price: number,
+    category?: "Electronic" | "Cloths" | "Beautify",
+    discount?: number
 }
 
 
-console.log(person.address)
+const product1: Products = {
+    name: "Boys T-shirt",
+    price: 200,
+    category: "Cloths"
+}
+
+
+const product2: Products = {
+    name: "Watch",
+    price: 500,
+    category: 'Electronic',
+    discount: 100
+}
+
+console.log(product1.price)
+console.log(product2.category)
